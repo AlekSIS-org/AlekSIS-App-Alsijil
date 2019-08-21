a9df90013aa7330fc2917517f8859f35dc950ebb
Dominik George <dominik.george@teckids.org>
2019-08-21 22:20:56 +0200

    Determine lesson period for lesson view. Advances #1.

--- biscuit/apps/alsijil/urls.py
+++ biscuit/apps/alsijil/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path('lesson', views.lesson, name='lesson'),
+    path('lesson/<int:week>/<int:period_id>', views.lesson,
+         name='lesson_by_week_and_period'),
 ]
--- biscuit/apps/alsijil/views.py
+++ biscuit/apps/alsijil/views.py
@@ -2,7 +2,23 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.utils.translation import gettext_lazy as _
 
+from biscuit.apps.chronos.models import LessonPeriod
+from biscuit.apps.chronos.util import current_lesson_periods, current_week
+
 
 @login_required
-def lesson(request):
-    pass
+def lesson(request, week=None, period_id=None):
+    context = {}
+
+    if week and period_id:
+        lesson_period = LessonPeriod.objects.get(pk=period_id)
+        wanted_week = week
+    else:
+        lesson_period = current_lesson_periods().filter(
+            lesson__teachers=request.user.person).first()
+        wanted_week = current_week()
+
+    context['lesson_period'] = lesson_period
+    context['week'] = wanted_week
+
+    return render(request, 'alsijil/lesson.html', context)
