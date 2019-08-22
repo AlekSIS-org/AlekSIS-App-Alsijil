c4f9f9401ab672976bad62be4cb30d932407cdb5
Dominik George <nik@naturalnet.de>
2019-08-22 22:53:02 +0200

    Remove unnecessary import.

--- biscuit/apps/alsijil/models.py
+++ biscuit/apps/alsijil/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-from biscuit.apps.chronos.util import current_week
 from biscuit.core.mixins import SchoolRelated
 
 
