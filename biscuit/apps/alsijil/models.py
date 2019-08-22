4ac24dfd942c4f7bb35100ca034a909bf808debc
Dominik George <nik@naturalnet.de>
2019-08-22 22:44:14 +0200

    Fix wrong import.

--- biscuit/apps/alsijil/models.py
+++ biscuit/apps/alsijil/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-from biscuit.chronos.util import current_week
+from biscuit.apps.chronos.util import current_week
 from biscuit.core.mixins import SchoolRelated
 
 
