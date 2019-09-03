537af657858c0362a56112c8cc085e094e04665a
Dominik George <dominik.george@teckids.org>
2019-09-03 09:50:04 +0200

    Ignore inactive persons in class lists.

--- biscuit/apps/alsijil/views.py
+++ biscuit/apps/alsijil/views.py
@@ -40,7 +40,7 @@ def lesson(request: HttpRequest, week: Optional[int] = None, period_id: Optional
 
         # Create all missing personal notes about members of all groups in lesson
         for group in lesson_period.lesson.groups.all():
-            for person in group.members.all():
+            for person in group.members.filter(is_active=True):
                 note, created = PersonalNote.objects.get_or_create(person=person, lesson_period=lesson_period,
                                                                    week=wanted_week)
 
