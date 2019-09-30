261868273f5aa9510a51cc94ebfda901404b9651
Tom Teichler <tom.teichler@teckids.org>
2019-09-30 22:12:44 +0200

    Rename url.

--- biscuit/apps/alsijil/urls.py
+++ biscuit/apps/alsijil/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('lesson', views.lesson, name='lesson'),
     path('lesson/<int:year>/<int:week>/<int:period_id>', views.lesson,
          name='lesson_by_week_and_period'),
-    path('group/week', views.week_view, name='week_view'),
-    path('group/week/<int:year>/<int:week>', views.week_view,
+    path('week', views.week_view, name='week_view'),
+    path('week/<int:year>/<int:week>', views.week_view,
          name='week_view_by_week'),
     path('print/group/<int:id_>', views.full_register_group,
         name='full_register_group')
