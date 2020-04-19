4bd42763a6529da023e6b61876586e0fdde1362b
Jonathan Weth <git@jonathanweth.de>
2020-04-19 19:47:55 +0200

    Update copyright information and add new app config meta data

--- aleksis/apps/alsijil/apps.py
+++ aleksis/apps/alsijil/apps.py
@@ -3,4 +3,14 @@ from aleksis.core.util.apps import AppConfig
 
 class AlsijilConfig(AppConfig):
     name = "aleksis.apps.alsijil"
-    verbose_name = "AlekSIS - Alsijil (Class register)"
+    verbose_name = "AlekSIS — Alsijil (Class register)"
+
+    urls = {
+        "Repository": "https://edugit.org/AlekSIS/official/AlekSIS-App-Alsijil/",
+    }
+    licence = "EUPL-1.2+"
+    copyright = (
+        ([2019], "Dominik George", "dominik.george@teckids.org"),
+        ([2019], "mirabilos", "thorsten.glaser@teckids.org"),
+        ([2019], "Tom Teichler", "tom.teichler@teckids.org"),
+    )
