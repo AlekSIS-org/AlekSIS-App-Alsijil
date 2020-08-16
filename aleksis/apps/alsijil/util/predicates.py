b422dae3841272943ec6f500dc4b1b7f0bea6d1a
Jonathan Weth <git@jonathanweth.de>
2020-08-16 12:35:29 +0200

    Fix query

--- aleksis/apps/alsijil/util/predicates.py
+++ aleksis/apps/alsijil/util/predicates.py
@@ -192,6 +192,6 @@ def has_any_object_absence(user: User) -> bool:
     Predicate which builds a query with all the persons the given users is allowed to register an absence for.
     """
     return get_objects_for_user(user, "core.register_absence_person", Person)\
-        .union(Person.objects.filter(member_of__owners=user))\
+        .union(Person.objects.filter(member_of__owners=user.person))\
         .union(Person.objects.filter(member_of__in=get_objects_for_user(user, "core.register_absence_group", Group)))\
         .exists()
