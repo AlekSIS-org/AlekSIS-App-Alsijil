e26e30cb4b50c0c3ab698ac60d5e9fabaf47c05b
Julian Leucker <leuckerj@gmail.com>
2020-09-11 15:29:42 +0200

    Fix bug when no tardinesses exist

--- aleksis/apps/alsijil/templatetags/time_helpers.py
+++ aleksis/apps/alsijil/templatetags/time_helpers.py
@@ -8,7 +8,7 @@ register = template.Library()
 @register.filter("to_time")
 def get_time_from_minutes(minutes: int) -> datetime.timedelta:
     """Get a time object from a number of minutes."""
-    delta = datetime.timedelta(minutes=minutes)
+    delta = datetime.timedelta(minutes=(minutes or 0))
     time_obj = (datetime.datetime.min + delta).time()
 
     return time_obj
