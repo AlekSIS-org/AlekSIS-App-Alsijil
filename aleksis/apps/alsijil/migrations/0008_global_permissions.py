7479aefcba1e084e63f0719c3199fb4fe27f361e
Tom Teichler <tom.teichler@teckids.org>
2021-04-08 22:24:18 +0200

    Prepare django 3.2 update

--- aleksis/apps/alsijil/migrations/0008_global_permissions.py
+++ aleksis/apps/alsijil/migrations/0008_global_permissions.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='AlsijilGlobalPermissions',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('extended_data', models.JSONField(default=dict, editable=False)),
             ],
             options={
